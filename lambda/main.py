eb6824f626cc94f418105afe2562d04b486434a0
kehinde <oyedepokehinde2016@gmail.com>
2024-10-27 20:24:21 +0100

    feat(ci-cd): setup ci-cd workflow

--- lambda/main.py
+++ lambda/main.py
@@ -1,7 +1,9 @@
+import os
 
 def handler(event, context):
+    version = os.environ.get('VERSION', "1.0")
     response_body = {
         "message": "Hello World!",
-        "version": "1.0.0"
+        "version": version
     }
     return { "statusCode": 200, "body": response_body }
--- lib/awscicd-stack.ts
+++ lib/awscicd-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as dotenv from 'dotenv'
 
 export class AwscicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //load the environment .env file
+    dotenv.config();
+
     //setup lambda
     const lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset('lambda'),
-      handler: 'main.handler'
+      handler: 'main.handler',
+      environment: {
+        VERSION: process.env.VERSION || "0.0"
+      }
     })
 
     //to make the lambda accessible, create a functionUrl
