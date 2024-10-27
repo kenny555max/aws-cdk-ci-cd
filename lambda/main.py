07a1cc60f06beab1b067f88edcc84f6dd2e7b72d
kehinde <oyedepokehinde2016@gmail.com>
2024-10-27 20:54:55 +0100

    feat(ci-cd): updated ci-cd workflow to cache Nodejs dependencies

--- lambda/main.py
+++ lambda/main.py
@@ -1,9 +1,32 @@
 import os
+import boto3 # aws-sdk in python
 
 def handler(event, context):
-    version = os.environ.get('VERSION', "1.0")
+    # Raw event data.
+    # prevent favicon request when the browser loads
+    path = event["rawPath"]
+    if path != "/":
+        return {"statusCode": 404, "body": "Not found."}
+
+    # Get a reference to the DDB table.
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ.get("TABLE_NAME"))
+
+    # Read the "VISIT COUNT" key (or create it if it doesn't exist)
+    response = table.get_item(Key={"key": "visit_count"})
+    if "Item" in response:
+        visit_count = response["Item"]["value"]
+    else:
+        visit_count = 0
+
+    # Increment the visit count and write it back to the table.
+    new_visit_count = visit_count + 1
+    table.put_item(Item={"key": "visit_count", "value": new_visit_count})
+
+    version = os.environ.get("VERSION", "0.0")
     response_body = {
-        "message": "Hello World!",
-        "version": version
+        "message": "Hello World 👋",
+        "version": version,
+        "visit_count": new_visit_count,
     }
     return { "statusCode": 200, "body": response_body }
--- lib/awscicd-stack.ts
+++ lib/awscicd-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as dotenv from 'dotenv'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as dotenv from 'dotenv';
 
 export class AwscicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,15 +11,27 @@ export class AwscicdStack extends cdk.Stack {
     //load the environment .env file
     dotenv.config();
 
+    // Create a table to store some data.
+    const table = new dynamodb.Table(this, "VisitorTimeTable", {
+      partitionKey: {
+        name: "key",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // when user performs any CRUD action
+    });
+
     //setup lambda
     const lambdaFunction = new lambda.Function(this, 'LambdaFunction', {
       runtime: lambda.Runtime.PYTHON_3_9,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'main.handler',
       environment: {
-        VERSION: process.env.VERSION || "0.0"
+        VERSION: process.env.VERSION || "0.0",
+        TABLE_NAME: table.tableName
       }
     })
+
+    table.grantReadWriteData(lambdaFunction);
 
     //to make the lambda accessible, create a functionUrl
     //lambda has an in-built function to make that happen
