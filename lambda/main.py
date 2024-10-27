65ec1ccf314ee993143767938f01824085256801
kehinde <oyedepokehinde2016@gmail.com>
2024-10-27 19:47:17 +0100

    feat(ci-cd): setup ci-cd workflow

--- lambda/main.py
+++ lambda/main.py
@@ -1,3 +1,7 @@
 
 def handler(event, context):
-    return { "statusCode": 200, "body": "Hello Worled" }
+    response_body = {
+        "message": "Hello World!",
+        "version": "1.0.0"
+    }
+    return { "statusCode": 200, "body": response_body }
